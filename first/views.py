04e4b7372a76b412b869eb05e5b2d1fc6e04431e
wangyao <wangyao.dream@hotmail.com>
2023-10-31 17:54:37 +0800

    add templates

--- first/views.py
+++ first/views.py
@@ -1,3 +1,15 @@
+from random import sample
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def show_index(r):
+    fruits = [
+            'Apple', 'Orange', 'Pitaya', 'Durian', 'Waxberry', 'Blueberry',
+            'Grape', 'Peach', 'Pear', 'Banana', 'Watermelon', 'Mango'
+        ]
+    selected_fruits = sample(fruits, 3)
+    return render(r, 'index.html', {'fruits': selected_fruits})
+
--- myweb/urls.py
+++ myweb/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from first.views import show_index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', show_index),
+
 ]
